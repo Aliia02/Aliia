93a6a54f11cc6035fd78180bd152b16b374df04d
Aliia02 <80500210+Aliia02@users.noreply.github.com>
2021-03-12 10:22:46 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 <div align="center" >
   <p> Aliia's CV</p>
 </div>
+ <img src="photo.jpg" alt="Girl in a jacket" style="width:266px;height:234px;"> 
 <p> <strong> Full name:Aliia Talaibekova <br> <br>
 Age:19 <br> <br>
 Date of birth:16.03.2002 <br> <br>
 Major: computer science <br> <br>
 City: Bishkek <br> <br>
 About me: My name is Aliia. I chose computer science because A programmer is, in a sense, a very creative person. He needs to quickly find the correct and shortest solution, find and correct mistakes <strong>.
+
